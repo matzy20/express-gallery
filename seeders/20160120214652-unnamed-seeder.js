4cb21054999577bff2990e79a0af2c8e005ff0fa
Gail Matsushima <gail.matsushima@gmail.com>
2016-01-21 18:51:43 -1000

    pulling 10 galleries and specifi id

--- seeders/20160120214652-unnamed-seeder.js
+++ seeders/20160120214652-unnamed-seeder.js
@@ -3,17 +3,22 @@ var faker = require('faker');
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-      //this needs to match name in database
-      return queryInterface.bulkInsert('Galleries', [
-        {
-          author: faker.name.firstName(),
-          link: faker.image.imageUrl(),
-          description: faker.lorem.sentence(),
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-      ], {});
-
+    var photos = [];
+    //for loop needs to happen before return
+    for (var i = 0; i < 10; i ++){
+      //made photo into an object to be pushed into array
+      var photo = {
+        author: faker.name.firstName(),
+        link: faker.image.imageUrl(),
+        description: faker.lorem.sentence(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      };
+      photos.push(photo);
+    }
+    //returns a code that allows bulkInsert and query to run, passing through array
+    //'Galleries' needs to match name in database
+    return queryInterface.bulkInsert('Galleries', photos);
   },
 
   down: function (queryInterface, Sequelize) {
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var db = require('./models');
 var express = require('express');
 var app = express();
 var path = require('path');
+app.set('view engine', 'jade');
 
 //be sure to npm install body-parser
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:false}));
-// app.set('view engine', 'jade');
 
 // app.set('views', path.resolve(_dirname, 'views'));
 
@@ -16,8 +16,9 @@ app.use(bodyParser.urlencoded({extended:false}));
 //   res.render('index', data);
 // });
 
-app.get('/galleries', function (req, res){
-  //needs to match model gallery 'string' which is capitalized
+app.get('/', function (req, res){
+  //db.Gallery needs to match model gallery 'string' which is capitalized
+  /*thought this would return more than one galleries? Yep, needed to code in seeder*/
   db.Gallery.findAll({}).then(function(galleries){
     res.json(galleries);
   });
@@ -27,7 +28,9 @@ app.get('/galleries/:id', function (req, res){
   //find is for one gallery vs all
   db.Gallery.find({
     where: {
-      id: 2
+      /*three different query parameters (req objects on express) = params query and body, chose params because it's as a URL
+      id is a property on a parameter*/
+      id: req.params.id
     }
   })
   .then(function(galleries){
@@ -35,6 +38,9 @@ app.get('/galleries/:id', function (req, res){
   });
 });
 
+/*1. post is end point for new;
+  2. which will be displayed in views/jade vs postman
+*/
 
 
 //sync to database
