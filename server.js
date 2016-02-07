cc2f1e7eb25e2bce333134225b1153a1a8ee49da
Gail Matsushima <gail.matsushima@gmail.com>
2016-02-06 15:05:26 -1000

    refinded server removed unecessary views get route

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ var session = require('express-session');
 var LocalStrategy = require('passport-local').Strategy;
 var config = require('./config');
 
-//be sure to npm install body-parser
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 
@@ -17,7 +16,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(session(config.session));
 
-//morgan takes in a string and use dev settings
 app.use(morgan('dev'));
 app.use(methodOverride('_method'));
 app.use(passport.initialize());
@@ -25,11 +23,11 @@ app.use(passport.session());
 
 //need to reference to config.json vs hardcoding properties on user
 // var user = config.CREDENTIALS;
-//we removed credentials though on config, which is why commented out var user
+//removed credentials on config, which is why commented out var user
 passport.use(new LocalStrategy(
   function (username, password, done){
     //links to models database and matches to username/pword passed in
-    //now can only login using users defined in User database
+    //logging in using users defined in User database
     db.User.find({
       where:{
         username: username,
@@ -76,25 +74,19 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', 'views');
 app.set('view engine', 'jade');
 
-//returning all galleries in gallery
+//returns all galleries in gallery
+//model gallery needs to be plural since findAll
 app.get('/', function (req, res){
   //db.Gallery needs to match model gallery 'string' which is capitalized
   /*thought this would return more than one galleries? Yep, needed to code in seeder*/
   return db.Gallery.findAll({}).then(function(galleries){
-    //to replace res.json (used for the seeders) with res.render when creating forms
-    // res.json(galleries);
-    // console.log(galleries);
-    /*and since finding ALL, gallery (the model; singular), will be plural
-    (returning an array of object galleries)
-    */
     res.render('index-gallery', {
-      /*need to reference since pulling existing info/galleries in
-      vs gallery/new where info is entered*/
+      //pulling existing galleries
       'galleries': galleries
     });
   });
 });
-//needs to be before the gallery/:id due to order
+//needs to be before gallery/:id due to order
 //creates a new gallery
 //isAuthenticated making sure new gallery is a valid user
 app.get('/gallery/new',
@@ -111,8 +103,7 @@ app.get('/login', function (req, res){
 
 //grabs gallery via seeders via faker by /gallery/id#
 app.get('/gallery/:id', function (req, res){
-
-  //find() is for one gallery vs all
+  //find specific Gallery
   db.Gallery.find({
     where: {
       /*three different query parameters (req objects on express) = params query and body, chose params because it's as a URL and
@@ -121,8 +112,8 @@ app.get('/gallery/:id', function (req, res){
     }
   //find one gallery, so singular
   }).then(function(gallery){
-    //to replace res.json (used for seeders/fakers) with res.render when creating forms
-    // res.json(gallery);
+
+    // res.render vs res.json(gallery);
     res.render('gallery', {
       'author': gallery.author,
       'id': gallery.id,
@@ -131,7 +122,8 @@ app.get('/gallery/:id', function (req, res){
     });
   });
 });
-//edits gallery, submit brings you to new page with updates
+//edits gallery
+//submit brings you to new page with updates
 app.get('/gallery/:id/edit',
   isAuthenticated,
   function (req, res){
@@ -166,26 +158,12 @@ app.get('/gallery/:id/delete', function (req, res){
   });
 });
 
-// what is this doing? brings you to a form to create a new gallery
-app.get('/views', function (req, res){
-  //get data from database
-  //rendering jade file
-  res.render('new-gallery', function (err, html){
-    // console.log('wassup');
-     if(err){
-      res.status = 404;
-      return res.send('Error Error');
-     }
-     res.send(html);
-  });
-});
-
 app.get('/logout', function (req, res){
   req.logout();
   res.redirect('/');
 });
 
-//posts new gallery to
+//posts new gallery
 app.post('/gallery', function (req, res){
   db.Gallery.create({
     author: req.body.author,
@@ -212,20 +190,11 @@ app.post('/login',
   })
 );
 
-//post users
-// app.post('/user', function (req, res){
-//   db.User.create({
-//     username: '',
-//     password: ''
-//   }).then(function(user){
-//       res.json(user);
-//     });
-// });
-//just need id, no need edit to match what's in browser
+//just need id, no need to match url in browser
 app.put('/gallery/:id', function (req, res){
   console.log('not working?');
   //update takes in two parameters searching by id
-  //so included a where, similar to a find
+  //where is similar to a find
   db.Gallery.update({
     author: req.body.author,
     link: req.body.link,
@@ -236,7 +205,7 @@ app.put('/gallery/:id', function (req, res){
     }
   }).then(function(gallery){
     //not render, bc that will prompt a 'get' to be override into a 'put'
-    //look at network tab to see what type of requests being sent
+    //network and headers on types of requests
     //need a redirect
     res.redirect('/gallery/' + req.params.id);
   });
@@ -244,7 +213,7 @@ app.put('/gallery/:id', function (req, res){
 //destroy takes in one parameter
 /*don't use TRUNCATE, removes everything
 had to re-run seeders, and change loop from < 55 to > 55
-since truncate removed everything*/
+since truncate removed everything, then changed back see photo.js*/
 app.delete('/gallery/:id', function (req, res){
   console.log(req.params);
   db.Gallery.destroy({
@@ -256,6 +225,25 @@ app.delete('/gallery/:id', function (req, res){
   });
 });
 
+//Catch-all "route undefinded" handler using middleware
+app.use(function (req, res, next){
+  //400 = client error, YOU referenced wrong page
+  res.status(404);
+  //uses a message vs your tmi computer info
+  next();
+  return res.send('Client, YOU made mistake' + err);
+});
+
+//Default catch-all middleware error handler
+app.use(function (err, req, res, next){
+  if(err){
+    //server error
+    res.status(500);
+    next();
+    return res.send('Something bad happened ' + err);
+  }
+});
+
 /*1. post is end point for new;
   2. which will be displayed in views/jade vs postman
   3. a form can only do one type of request
@@ -263,7 +251,6 @@ app.delete('/gallery/:id', function (req, res){
   5. add another get request for /gallery/:id/edit gets posted with edit-gallery.jade?
   6. using gulpfile.js to use sass/css; gulp and nodemon running same time; task compile-sass;
 */
-
 
 //sync to database
 db.sequelize
